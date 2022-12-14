b60a702ac10ba80f6caa2244a5731fd7dfc1bef3
Pedro Fernandes <pjf_fernandes@yahoo.com.br>
2022-12-13 22:53:55 -0300

    drawer added

--- lib/components/main_drawer.dart
+++ lib/components/main_drawer.dart
@@ -6,11 +6,30 @@ class MainDrawer extends StatelessWidget {
   const MainDrawer({super.key});
 
   Widget _createDrawerItem(IconData icon, String label, Function onTap) {
-    return ListTile();
+    return ListTile(
+      leading: Icon(icon),
+      title: Text(label),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Drawer();
+    return Drawer(
+      backgroundColor: Colors.white,
+      child: Column(
+        children: [
+          Container(
+            color: Theme.of(context).primaryColor,
+            height: 55,
+            width: double.infinity,
+            child: Text(
+              "Configurações",
+              style: TextStyle(color: Colors.white, fontSize: 18),
+            ),
+          ),
+          _createDrawerItem(Icons.person, "Editar conta", () => {}),
+        ],
+      ),
+    );
   }
 }
