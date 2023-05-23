775f38b9a179cbc4fafaa1c20e367eb782a05a9c
Pedro Fernandes <pjf_fernandes@yahoo.com.br>
2023-05-23 14:34:04 -0300

    build

--- lib/components/location_input.dart
+++ lib/components/location_input.dart
@@ -72,6 +72,18 @@ class _LocationInputState extends State<LocationInput> {
   }
 
   Future<void> _selectOnMap(Point point) async {
+    permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
     final locData = await Geolocator.getCurrentPosition();
 
     setState(() {
